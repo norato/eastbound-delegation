123939777d43b6fcf03f81d10f27cf66f63b03c9
Ryan T. Hosford <tad.hosford@gmail.com>
2016-06-14 10:20:32 -0500

    Latest logos/images

--- app/helpers/delegates_helper.rb
+++ app/helpers/delegates_helper.rb
@@ -7,11 +7,11 @@ module DelegatesHelper
 
   def share_me(delegate = nil)
     d = @delegate || delegate
-    "Help send Bernie's Nat'l Delegate to convention in Philly:\n#{truncate(pretty_name(d), length: 18, omission: '…')}"
+    "Pls RT & share! Band together, help send Bernie's delegate to Philly:\n#{truncate(pretty_name(d), length: 18, omission: '…')}"
   end
 
   def share_state(state)
-    "Pls RT/share: Adopt a Bernie Delegate from #{state}, make sure we're all represented in Philly"
+    "Pls RT & share! Let's band together & get Bernie delegates to Philly! www.adoptaberniedelegate.com"
   end
 
   def state_url(state)
