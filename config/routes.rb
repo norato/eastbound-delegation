65503d35144135b0b2d4bb638dfcdfdcb79c9b1c
Ryan T. Hosford <tad.hosford@gmail.com>
2016-06-14 01:50:31 -0500

    Latest

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :delegates, only: [:show, :index]
 
-  get "states/:state/delegates" => "delegates#index"
+  get "states/:state/delegates" => "delegates#index", as: :state_delegates
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "delegates#index"
 end
